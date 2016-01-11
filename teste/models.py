064dd39ec209856239bda6af208111149b0723e6
ltoyoda <ltoyoda@hotmail.com>
2016-01-11 17:08:43 +0100

    My Django Girls app, first commit

---
+++ teste/models.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+from django.utils import timezone
+
+
+class Page(models.Model):
+    author = models.ForeignKey('auth.User')  # this is a link to another model.
+    title = models.CharField(max_length=200) # this is how you define text with a limited number of characters.
+    text = models.TextField() # this is for long text without a limit. Sounds ideal for blog post content, right?
+    created_date = models.DateTimeField(
+            default=timezone.now)   # this is a date and time.
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
