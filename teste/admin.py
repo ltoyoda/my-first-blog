a57017676dd022b81750862a265aae1556b063c0
ltoyoda <ltoyoda@hotmail.com>
2016-02-02 15:06:59 +0100

    Changed the HTML for the site.

--- teste/admin.py
+++ teste/admin.py
@@ -6,28 +6,6 @@ from .models import Post
 from django.contrib import admin
 # from polls.models import Choice, Question
 
+
 admin.site.register(Post)  # Importing the post model (from models.py defined before)
 
-
-################################################
-
-# Done again with Polls added:
-#
-# class ChoiceInline(admin.TabularInline): # one below the previous
-#     model = Choice
-#     extra = 3
-#
-#
-# class QuestionAdmin(admin.ModelAdmin):
-#     fieldsets = [
-#         (None,               {'fields': ['question_text']}),
-#         ('Date information', {'fields': ['pub_date']}),
-#
-#     ]
-#     list_display = ('question_text', 'pub_date')
-#     list_display = ('question_text', 'pub_date', 'was_published_recently')
-#     list_filter = ['pub_date']
-#     search_fields = ['question_text']
-#
-# admin.site.register(Question, QuestionAdmin)
-# # admin.site.register(Choice)
